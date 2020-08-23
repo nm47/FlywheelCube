15399e5329837a7cbfb56c9a04cba1faa94e0ad3
Niels Moeller <Nielsmoeller9@gmail.com>
2020-08-23 02:09:48 -0500

    Fixed derivative kick

--- src/PID.cpp
+++ src/PID.cpp
@@ -1,28 +1,38 @@
 
 // Arduino PID implementation by Niels Moeller
-
 #include "PID.h"
 
 
-void PID::SetTunings(double p, double i, double d){
+void PID::SetTunings(float p, float i, float d){
 	kp = p;
-	ki = i;
-	kd = d;
+	ki = i * ((float)sampletime / 1000); //convert sample time to seconds
+	kd = d / ((float)sampletime / 1000);
 }
 
 void PID::compute(){
 	currenttime = millis();
-	double dt = (double)(currenttime - lasttime);
+	float dt = (float)(currenttime - lasttime);
 
-	//Calculate the current error.
-	error = (setpoint - input);
-	errSum = error * dt;
-	//The derivative of the error with respect to time.
-	derE = (error - lasterr) / dt;
+	if (dt >= sampletime) {
+		//Calculate the current error.
+		error = (setpoint - input);
+		errSum += error;
 
-	output = kp * error + ki * errSum + kd * derE;
+		//change in input since last timestep
+		float dinp = input - lastinput;
 
-	lastErr = error;
-	lastTime = currenttime;
+		output = kp * error + ki * errSum - kd * dinp;
+
+		lastinput = input;
+		lastTime = currenttime;
+	}
+}
+void PID::SetSampleTime(const int stime) {
+	if (stime > 0) {
+		sampletime = (unsigned long)stime;
+		float ratio = (float)stime / (float)sampletime;
+		ki *= ratio;
+		kd /= ratio;
+	}
 }
 
--- src/PID.h
+++ src/PID.h
@@ -2,17 +2,19 @@
 
 class PID{
 	private:
-		double input, double  output, double setpoint;
-		double kp, double ki, double kd;
-		double errSum, lastErr;
+		float input, float  output, float setpoint;
+		float kp, float ki, float kd;
+		float errSum, lastErr, lastinput;
+		int sampletime = 500; 
 		unsigned long lasttime, currenttime;
 	public:
-		PID(double &inp, double &outp, double &setp){
+		PID(double &inp,  &outp, float &setp){
 			currenttime = 0;
 			input = inp;
 			output = outp;
 			setpoint = setp;
 		};
-		void SetTunings(double kp, double ki, double kd);
+		void SetTunings(float kp, float ki, float kd);
+		void SetSampleTime(const int stime);
 		void compute();
 };
